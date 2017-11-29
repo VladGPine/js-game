31be77080980b4cee3860c272e5d8abc8e1ad469
Vladimir Golodaev <golodaevvladimir@gmail.com>
2017-11-29 13:41:33 +0300

    class Actor done. isIntersect method need to fix. Objects didn't overlap partially

--- game.js
+++ game.js
@@ -1,143 +1,75 @@
 'use strict';
 
 class Vector {
-    constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
-    }
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	}
 
-    plus (vectorObj) {
-        try {
-            if (!vectorObj instanceof Vector) {
-                throw 'Можно прибавлять к вектору только вектор типа Vector';
-            } 
-        } catch (e) {
-            console.log(e);
-        }
-        
-        let vector = new Vector(this.x + vectorObj.x, this.y + vectorObj.y);
-        // console.log(vector);
-        return vector;
-    }
+	plus(vectorObj) {
+		if (!(vectorObj instanceof Vector)) throw new Error('Можно прибавлять к вектору только вектор типа Vector');
+		let vector = new Vector(this.x + vectorObj.x, this.y + vectorObj.y);
+		return vector;
+	}
 
-    times (multiplier) {
-        let vector = new Vector(this.x * multiplier, this.y * multiplier);
-        return vector;
-    }
+	times(multiplier) {
+		let vector = new Vector(this.x * multiplier, this.y * multiplier);
+		return vector;
+	}
 }
 
 class Actor {
-    constructor() {
-        const position = new Vector(pos);
-        
+	constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+		if (!(pos instanceof Vector)) throw new Error('Неопределенный тип, отличный от объекта типа Vector');
+		if (!(size instanceof Vector)) throw new Error('Неопределенный тип, отличный от объекта типа Vector');
+		if (!(speed instanceof Vector)) throw new Error('Неопределенный тип, отличный от объекта типа Vector');
+		this.pos = pos;
+		this.size = size;
+		this.speed = speed;
+		Object.defineProperty(this, 'left', {
+			value: Math.min(this.pos.x, this.pos.x + this.size.x),
+			writable: false,
+			configurable: false,
+			enumerable: true
+		});
+		Object.defineProperty(this, 'right', {
+			value: Math.max(this.pos.x, this.pos.x + this.size.x),
+			writable: false,
+			configurable: false,
+			enumerable: true
+		});
+		Object.defineProperty(this, 'top', {
+			value: Math.min(this.pos.y, this.pos.y + this.size.y),
+			writable: false,
+			configurable: false,
+			enumerable: true
+		});
+		Object.defineProperty(this, 'bottom', {
+			value: Math.max(this.pos.y, this.pos.y + this.size.y),
+			writable: false,
+			configurable: false,
+			enumerable: true
+		});
+		Object.defineProperty(this, 'type', {
+			value: 'actor',
+			writable: false,
+			configurable: false,
+			enumerable: true
+		});
+	}
 
-    }
-    act () {
+	isIntersect(movingObj = new Actor()) {
+		if (!(movingObj instanceof Actor)) throw new Error('Неопределенный тип, отличный от объекта типа Actor');
+		if (movingObj === this) return false;
+		if (movingObj.left >= this.left || movingObj.left >= this.right) return false;
+		if (movingObj.right <= this.right || movingObj.right <= this.left) return false;
+		if (movingObj.top >= this.top || movingObj.top >= this.bottom) return false;
+		if (movingObj.bottom <= this.bottom || movingObj.bottom <= this.top) return false;
+		// if (movingObj.left >= this.left || movingObj.right <= this.right || movingObj.top >= this.top || movingObj.bottom <= this.bottom) return false;
+		return true;
+	}
 
-    }
+	act() {
+
+	}
 }
-
-// const grid = [
-//   new Array(3),
-//   ['wall', 'wall', 'lava']
-// ];
-// const level = new Level(grid);
-// runLevel(level, DOMDisplay);
-
-// const schema = [
-//   '         ',
-//   '         ',
-//   '         ',
-//   '         ',
-//   '     !xxx',
-//   '         ',
-//   'xxx!     ',
-//   '         '
-// ];
-// const parser = new LevelParser();
-// const level = parser.parse(schema);
-// runLevel(level, DOMDisplay);
-
-// const schema = [
-//   '         ',
-//   '         ',
-//   '         ',
-//   '         ',
-//   '     !xxx',
-//   ' @       ',
-//   'xxx!     ',
-//   '         '
-// ];
-// const actorDict = {
-//   '@': Player
-// }
-// const parser = new LevelParser(actorDict);
-// const level = parser.parse(schema);
-// runLevel(level, DOMDisplay);
-
-// const schema = [
-//   '         ',
-//   '         ',
-//   '    =    ',
-//   '         ',
-//   '     !xxx',
-//   ' @       ',
-//   'xxx!     ',
-//   '         '
-// ];
-// const actorDict = {
-//   '@': Player,
-//   '=': HorizontalFireball
-// }
-// const parser = new LevelParser(actorDict);
-// const level = parser.parse(schema);
-// DOMDisplay(document.body, level);
-
-// const schema = [
-//   '         ',
-//   '         ',
-//   '    =    ',
-//   '       o ',
-//   '     !xxx',
-//   ' @       ',
-//   'xxx!     ',
-//   '         '
-// ];
-// const actorDict = {
-//   '@': Player,
-//   '=': HorizontalFireball
-// }
-// const parser = new LevelParser(actorDict);
-// const level = parser.parse(schema);
-// runLevel(level, DOMDisplay)
-//   .then(status => console.log(`Игрок ${status}`));
-
-// const schemas = [
-//   [
-//     '         ',
-//     '         ',
-//     '    =    ',
-//     '       o ',
-//     '     !xxx',
-//     ' @       ',
-//     'xxx!     ',
-//     '         '
-//   ],
-//   [
-//     '      v  ',
-//     '    v    ',
-//     '  v      ',
-//     '        o',
-//     '        x',
-//     '@   x    ',
-//     'x        ',
-//     '         '
-//   ]
-// ];
-// const actorDict = {
-//   '@': Player,
-//   'v': FireRain
-// }
-// const parser = new LevelParser(actorDict);
-// runGame(schemas, parser, DOMDisplay)
-//   .then(() => console.log('Вы выиграли приз!'));
