6a44bd3ccc4873ca5bc014ab61111c810be6a4a3
Vladimir Golodaev <golodaevvladimir@gmail.com>
2017-11-29 15:08:34 +0300

    class Actor done. isIntersect method need to fix. Objects didn't overlap partially.Some small fixes.

--- game.js
+++ game.js
@@ -6,20 +6,11 @@ class Vector {
 		this.y = y;
 	}
 
-<<<<<<< HEAD
 	plus(vectorObj) {
 		if (!(vectorObj instanceof Vector)) throw new Error('Можно прибавлять к вектору только вектор типа Vector');
 		let vector = new Vector(this.x + vectorObj.x, this.y + vectorObj.y);
 		return vector;
 	}
-=======
-    plus (vectorObj) {
-        if (!(vectorObj instanceof Vector)) throw new Error('Можно прибавлять к вектору только вектор типа Vector'); 
-        let vector = new Vector(this.x + vectorObj.x, this.y + vectorObj.y);
-        // console.log(vector);
-        return vector;
-    }
->>>>>>> 23af28450ed83e00790430d762dfb8bc7b6adb6d
 
 	times(multiplier) {
 		let vector = new Vector(this.x * multiplier, this.y * multiplier);
@@ -28,7 +19,6 @@ class Vector {
 }
 
 class Actor {
-<<<<<<< HEAD
 	constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
 		if (!(pos instanceof Vector)) throw new Error('Неопределенный тип, отличный от объекта типа Vector');
 		if (!(size instanceof Vector)) throw new Error('Неопределенный тип, отличный от объекта типа Vector');
@@ -82,24 +72,5 @@ class Actor {
 	act() {
 
 	}
-=======
-    constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
-        if (!(pos instanceof Vector)) throw new Error('Неопределенный тип, отличный от объекта типа Vector');
-        if (!(size instanceof Vector)) throw new Error('Неопределенный тип, отличный от объекта типа Vector');
-        if (!(speed instanceof Vector)) throw new Error('Неопределенный тип, отличный от объекта типа Vector');
-        this.pos = pos;
-        this.size = size;
-        this.speed = speed;
-        Object.defineProperty(this, 'type', {
-            value: 'actor',
-            writable: false,
-            configurable: false,
-            enumerable: true
-        });
-    }
 
-    act () {
-
-    }
->>>>>>> 23af28450ed83e00790430d762dfb8bc7b6adb6d
 }
